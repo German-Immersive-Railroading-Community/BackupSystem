2e06da495ed2af9ae15870df1abc572dc9d56c94
FGBxRamel <kretschmer.raziel@gmail.com>
2021-10-14 22:58:29 +0200

    Corrected the file zipping

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ftplib as ftp
 import hashlib as hl
 import json
 import os
+from zipfile import ZipFile
 
 from decouple import config
 
@@ -15,8 +16,7 @@ try:
         else:
             data[key] = value
 except FileNotFoundError:
-    print('Json file not found!')
-    exit()
+    pass
 except json.JSONDecodeError:
     print('The JSON is empty!')
     exit()
@@ -28,40 +28,67 @@ backfiles = []
 try:
     for line in open('backfiles.txt'):
         backfiles.append(line.replace('\n', ''))
-        print(backfiles)
 except FileNotFoundError:
     print('The file containing the files and folders that should be backed up has not been found!')
     exit()
 
 #Go trough the files, check if the file/folder exists in backfiles list
-#If exist: Check the files for their SHA and add folder/file to another list with full path for later zipping
+#If exist: Check the files for their SHA and add folder/file to the zip
 #Else: continue
 #Help for os.walk: https://docs.python.org/3/library/os.html#os.walk
-ziplist = []
+def add_zip(dir : str, zip : ZipFile, json_data : dict, backf : list):
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            if f in backf:
+                try:
+                    sha = hl.sha256(open(root + '/' + f, 'rb').read()).hexdigest()
+                    if sha == json_data['sha'][root + '/' + f]:
+                        continue
+                    else:
+                        json_data['sha'][root + '/' + f] = sha
+                        zip.write(root + '/' + f)
+                        backf.remove(f)
+                except KeyError:
+                    json_data['sha'][root + '/' + f] = sha
+                    zip.write(root + '/' + f)
+                    backf.remove(f)
+            else:
+                continue
+        for d in dirs:
+            if d in backf:
+                backf.remove(d)
+                json_data, backf = add_zip(root + '/' + d, zip, json_data, backf)
+    return json_data, backf
+
+
+zipfile = ZipFile('backup.zip', 'w')
 for root, dirs, files in os.walk(config('path')):
     combined = dirs + files
     for f in combined:
         if f in backfiles:
             try:
-                _file = open(root + '/' + f, 'rb').read()
-                sha = hl.sha256(_file).hexdigest()
-                if sha == data['sha'][f]:
+                sha = hl.sha256(open(root + '/' + f, 'rb').read()).hexdigest()
+                if sha == data['sha'][root + '/' + f]:
                     continue
                 else:
-                    ziplist.append(root + '/' + f)
-                    data['sha'][f] = sha
+                    zipfile.write(root + '/' + f)
+                    data['sha'][root + '/' + f] = sha
                     backfiles.remove(f)
             except IsADirectoryError:
-                ziplist.append(root + '/' + f + '/')
+                data, backfiles = add_zip(root + '/' + f + '/', zipfile, data, backfiles)
                 backfiles.remove(f)
             except KeyError:
-                data['sha'][f] = sha
-                ziplist.append(root + '/' + f)
+                data['sha'][root + '/' + f] = sha
+                zipfile.write(root + '/' + f)
                 backfiles.remove(f)
         else:
             continue
-print(ziplist)
-
+zipfile.close()
 with open('files.json', 'w') as outfile:
     json.dump(data, outfile)
+
+
 #Send the zip File to the Backupserver
+
+
+print('Backup made.')
