159ed6bea5072621c6c27d9054f415b12f61ec52
FGBxRamel <kretschmer.raziel@gmail.com>
2022-03-16 14:29:29 +0100

    Hotfix for the hotfix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ config = cp.ConfigParser()
 config.read("config.ini")
 config_variables = config["VARIABLES"]
 config_options = config["OPTIONS"]
-sha_check = config['OPTIONS']['sha_check'].getboolean()
+sha_check = config_options.getboolean('sha_check')
 
 #Set some time variables
 today = dt.today().strftime('%Y-%m-%d')
